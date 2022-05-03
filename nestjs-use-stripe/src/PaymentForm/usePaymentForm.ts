aa71490df12b475bfaea905ae743b57b4cfdedd7
huynhdanhlang <danhlangbmvl@gmail.com>
2022-05-03 16:05:34 +0700

    react app test charg and credit-cards api

--- nestjs-use-stripe/src/PaymentForm/usePaymentForm.ts
+++ nestjs-use-stripe/src/PaymentForm/usePaymentForm.ts
@@ -1,51 +1,66 @@
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import { FormEvent } from 'react';
- 
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { FormEvent } from "react";
+
 function usePaymentForm() {
   const stripe = useStripe();
   const elements = useElements();
- 
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
- 
-    const amountToCharge = 100;
- 
+
+  const getPaymentMethodId = async () => {
     const cardElement = elements?.getElement(CardElement);
- 
+
     if (!stripe || !elements || !cardElement) {
       return;
     }
- 
+
     const stripeResponse = await stripe.createPaymentMethod({
-      type: 'card',
-      card: cardElement
+      type: "card",
+      card: cardElement,
     });
- 
+
     const { error, paymentMethod } = stripeResponse;
- 
+
     if (error || !paymentMethod) {
       return;
     }
- 
-    const paymentMethodId = paymentMethod.id;
- 
-    fetch(`${process.env.REACT_APP_API_URL}/charge`, {
-      method: 'POST',
-      body: JSON.stringify(({
-        paymentMethodId,
-        amount: amountToCharge
-      })),
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
- 
+
+    return paymentMethod.id;
   };
- 
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const paymentMethodId = await getPaymentMethodId();
+
+    if (!paymentMethodId) {
+      return;
+    }
+
+    // api "/charge" thanh toan truc tiep
+    // api "credit-cards" them card credit
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/charge`, {
+      method: "POST",
+      body: JSON.stringify({
+        paymentMethodId,
+        amount: 100,
+      }),
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const responseJson = await response.json();
+    if (responseJson.status !== "succeeded") {
+      const clientSecret = responseJson.client_secret;
+      stripe?.confirmCardPayment(clientSecret); // dung the co san
+      // stripe?.confirmCardSetup(clientSecret); // thiet lap the moi
+
+    }
+  };
+
   return {
-    handleSubmit
-  }
+    handleSubmit,
+  };
 }
- 
+
 export default usePaymentForm;
