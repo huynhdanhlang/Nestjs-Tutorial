e787d190793639605dcb4fb4838c01c947e59284
huynhdanhlang <danhlangbmvl@gmail.com>
2022-05-03 16:44:37 +0700

    return data stripe

--- nestjs-use-stripe/src/PaymentForm/usePaymentForm.ts
+++ nestjs-use-stripe/src/PaymentForm/usePaymentForm.ts
@@ -50,11 +50,13 @@ function usePaymentForm() {
     });
 
     const responseJson = await response.json();
+    console.log(["secret"], responseJson);
+
     if (responseJson.status !== "succeeded") {
       const clientSecret = responseJson.client_secret;
-      stripe?.confirmCardPayment(clientSecret); // dung the co san
-      // stripe?.confirmCardSetup(clientSecret); // thiet lap the moi
 
+      await stripe?.confirmCardPayment(clientSecret); // dung the co san
+      // stripe?.confirmCardSetup(clientSecret); // thiet lap the moi
     }
   };
 
@@ -64,3 +66,58 @@ function usePaymentForm() {
 }
 
 export default usePaymentForm;
+
+/**
+ * 
+ * import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { FormEvent } from 'react';
+ 
+function usePaymentForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+ 
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+ 
+    const amountToCharge = 100;
+ 
+    const cardElement = elements?.getElement(CardElement);
+ 
+    if (!stripe || !elements || !cardElement) {
+      return;
+    }
+ 
+    const stripeResponse = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement
+    });
+ 
+    const { error, paymentMethod } = stripeResponse;
+ 
+    if (error || !paymentMethod) {
+      return;
+    }
+ 
+    const paymentMethodId = paymentMethod.id;
+ 
+    fetch(`${process.env.REACT_APP_API_URL}/charge`, {
+      method: 'POST',
+      body: JSON.stringify(({
+        paymentMethodId,
+        amount: amountToCharge
+      })),
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+ 
+  };
+ 
+  return {
+    handleSubmit
+  }
+}
+ 
+export default usePaymentForm;
+ */
