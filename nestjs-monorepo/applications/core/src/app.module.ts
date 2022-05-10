0d40b2ac749933ec7fe412b6b7ab22b0d2632b71
huynhdanhlang <danhlangbmvl@gmail.com>
2022-05-10 16:45:14 +0700

    Introduction to a monorepo with Lerna and Yern workspaces

---
+++ nestjs-monorepo/applications/core/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { EmailSubscriptionsModule } from './email-subscriptions/emailSubscriptions.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
+@Module({
+  imports: [
+    EmailSubscriptionsModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        SUBSCRIBERS_SERVICE_HOST: Joi.string().required(),
+        SUBSCRIBERS_SERVICE_PORT: Joi.number().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
